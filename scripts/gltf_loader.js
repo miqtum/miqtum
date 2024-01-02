7181a5ef7d11d1499e3bf528d7aec55c52aef585
miqtum <personinstance@gmail.com>
2024-01-02 11:16:20 +0300

    Update gltf_loader.js

--- scripts/gltf_loader.js
+++ scripts/gltf_loader.js
@@ -15,13 +15,13 @@ function init() {
     document.body.appendChild( container );
 
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-    camera.position.set( - 1.8, 0.6, 2.7 );
+    camera.position.set( 0, 2, 5 );
 
     scene = new THREE.Scene();
 
     new RGBELoader()
         .setPath( 'equirectangular/' )
-        .load( 'Space_8_sn.hdr', function ( texture ) {
+        .load( 'SPACE.hdr', function ( texture ) {
 
             texture.mapping = THREE.EquirectangularReflectionMapping;
 
@@ -47,18 +47,18 @@ function init() {
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 1;
+    renderer.toneMappingExposure = 2;
     container.appendChild( renderer.domElement );
 
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', render ); // use if there is no animation loop
-    controls.minDistance = 2;
-    controls.maxDistance = 10;
+    controls.minDistance = 5;
+    controls.maxDistance = 5;
     controls.target.set( 0, 0, - 0.2 );
     controls.enablePan = false;
     //controls.maxAzimuthAngle = THREE.MathUtils.degToRad(90);
-    //controls.minAzimuthAngle = THREE.MathUtils.degToRad(220);
-    //controls.minPolarAngle = THREE.MathUtils.degToRad(60);
+    controls.minAzimuthAngle = THREE.MathUtils.degToRad(220);
+    controls.minPolarAngle = THREE.MathUtils.degToRad(0);
     controls.update();
 
     window.addEventListener( 'resize', onWindowResize );
